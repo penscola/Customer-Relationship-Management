04b9cf73502315112a7738b8298dcfa1d2cffd3f
penscola <penscolashackletonfelix@gmail.com>
2024-05-01 16:46:34 +0300

    feat: user can be able to delete record

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -58,3 +58,14 @@ def customer_record(request, pk):
     else:
         messages.success(request, 'You must be Logged in to view the Page!!!')
         return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, 'Record deleted Successfully...')
+        return redirect('home')
+    else:
+        messages.success(request, 'You must be Logged in to view the Page!!!')
+        return redirect('home')
