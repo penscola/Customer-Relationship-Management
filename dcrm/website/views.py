011f60f8968045b6e898f946981df090c92cc110
penscola <penscolashackletonfelix@gmail.com>
2024-05-02 11:52:19 +0300

    feat: user is able to add record

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -66,6 +66,21 @@ def delete_record(request, pk):
         delete_it.delete()
         messages.success(request, 'Record deleted Successfully...')
         return redirect('home')
+    else:
+        messages.success(request, 'You must be Logged in to view the Page!!!')
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, 'Record Added ...')
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
     else:
         messages.success(request, 'You must be Logged in to view the Page!!!')
         return redirect('home')
